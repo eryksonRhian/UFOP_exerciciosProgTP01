682ca80a3ed0ac642aa5684f91be8a846294748d
eryksonRhian <eryksonrhian2@gmail.com>
2025-10-13 16:23:59 -0300

    Exercicios 15,16 e 17 da lista de exercícios, agora tem 21 restantes

---
+++ Exercicio_16/main.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int num;
+    scanf("%i", &num);
+    printf("O sucessor e o antecessor de %i são respectivamente %i e %i", num, num+1, num-1); 
+}
---
+++ Exercicio_17/main.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int n1,n2;
+    printf("Digite os dois números\n");
+    scanf("%i %i", &n1, &n2);
+    printf("SOMA = %i", n1+n2);
+}
